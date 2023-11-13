387e22ff57480bd90d3dcba4b9f474862f67d0cf
AdamUllmann <adamullmannbusiness@gmail.com>
2023-11-12 21:49:19 -0800

    Removed unwanted comments & cleaned Code for readability

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,24 +11,6 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class CustomLookAndFeel : public juce::LookAndFeel_V4 {
-public:
-    void drawButtonBackground(juce::Graphics& g, juce::Button& button,
-        const juce::Colour& backgroundColour,
-        bool shouldDrawButtonAsHighlighted,
-        bool shouldDrawButtonAsDown) override {
-        auto bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
-        auto baseColour = backgroundColour.withMultipliedSaturation(button.hasKeyboardFocus(true) ? 1.3f : 0.9f)
-            .withMultipliedAlpha(button.isEnabled() ? 1.0f : 0.5f);
-
-        if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
-            baseColour = baseColour.contrasting(shouldDrawButtonAsDown ? 0.2f : 0.05f);
-
-        g.setColour(baseColour);
-        g.fillRoundedRectangle(bounds, 6.0f); // Rounded corners
-    }
-};
-
 //==============================================================================
 /**
 */
